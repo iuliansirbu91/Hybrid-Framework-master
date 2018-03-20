8988c21ff95ed045a6525d2222b6a5cec726868a
iulisirbu <37597080+iulisirbu@users.noreply.github.com>
2018-03-21 00:04:47 +0200

    incercare UI de upload fisier

---
+++ src/main/java/FrameworkUI.java
@@ -0,0 +1,87 @@
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+ 
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JTextPane;
+ 
+public class FrameworkUI {
+ 
+  private JFrame frame;
+  private JTextField txtPath;
+ 
+  /**
+   * Launch the application.
+   */
+  public static void main(String[] args) {
+    EventQueue.invokeLater(new Runnable() {
+      public void run() {
+       try {
+    	   FrameworkUI window = new FrameworkUI();
+          window.frame.setVisible(true);
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      }
+    });
+  }
+ 
+  /**
+   * Create the application.
+   */
+  public FrameworkUI() {
+    initialize();
+  }
+ 
+  /**
+   * Initialize the contents of the frame.
+   */
+  private void initialize() {
+    frame = new JFrame();
+    frame.setBounds(100, 100, 450, 300);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.getContentPane().setLayout(null);
+         
+    txtPath = new JTextField();
+    txtPath.setBounds(10, 10, 414, 21);
+    frame.getContentPane().add(txtPath);
+    txtPath.setColumns(10);
+         
+    JButton btnBrowse = new JButton("Browse");
+    btnBrowse.setBounds(10, 41, 87, 23);
+    frame.getContentPane().add(btnBrowse);
+    
+    JTextPane textPane = new JTextPane();
+    textPane.setBounds(35, 103, 368, 121);
+    frame.getContentPane().add(textPane);
+         
+    btnBrowse.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+    	  
+    	  
+   // 	  panel.add(new JScrollPane(taskOutput), BorderLayout.CENTER);
+    //      MessageConsole mc = new MessageConsole(taskOutput);
+  
+          
+        JFileChooser fileChooser = new JFileChooser();
+ 
+        // For Directory
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+ 
+        // For File
+        //fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+ 
+        fileChooser.setAcceptAllFileFilterUsed(false);
+ 
+        int rVal = fileChooser.showOpenDialog(null);
+        if (rVal == JFileChooser.APPROVE_OPTION) {
+          txtPath.setText(fileChooser.getSelectedFile().toString());
+         
+        }
+      }
+    });
+  }
+}
